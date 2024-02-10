350c01500f9de51ca674783a18fecc9fe683dc5d
Sadiqhasan <sadiqhasanrupani11@gmail.com>
2024-02-11 01:00:34 +0530

    defined billing countries and user schemas

--- drizzle/schema.ts
+++ drizzle/schema.ts
@@ -1,16 +1,35 @@
-import { pgTable, serial, text } from "drizzle-orm/pg-core";
+import { pgTable, serial, text, foreignKey, integer } from "drizzle-orm/pg-core"
+  import { sql } from "drizzle-orm"
 
-export const demoTable = pgTable("demo_table", {
-  id: serial("id").primaryKey().notNull(),
-  demoName: text("demo_name").notNull(),
+
+
+export const card = pgTable("card", {
+	id: serial("id").primaryKey().notNull(),
+	title: text("title").notNull(),
+	description: text("description").notNull(),
+	groupName: text("group_name").notNull(),
 });
 
-export const anotherDemo = pgTable("another_demo", {
-  id: serial("id").primaryKey().notNull(),
-  demoNameId: serial("demo_name_id")
-    .notNull()
-    .references(() => demoTable.id, {
-      onDelete: "cascade",
-      onUpdate: "cascade",
-    }),
+export const users = pgTable("users", {
+	id: serial("id").primaryKey().notNull(),
+	username: text("username").notNull(),
+	firstName: text("first_name").notNull(),
+	lastName: text("last_name").notNull(),
+	email: text("email").notNull(),
+	mobileNo: text("mobile_no").notNull(),
+	profilePicture: text("profile_picture"),
+	bio: text("bio"),
+});
+
+export const boardUsers = pgTable("board_users", {
+	id: serial("id").primaryKey().notNull(),
+	userId: integer("user_id").references(() => users.id),
+	boardId: integer("board_id").references(() => boards.id),
+	permissionLevel: text("permission_level"),
+});
+
+export const boards = pgTable("boards", {
+	id: serial("id").primaryKey().notNull(),
+	name: text("name").notNull(),
+	background: text("background"),
 });
--- src/schema/schema.js
+++ src/schema/schema.js
@@ -1,14 +1,48 @@
-import { pgTable, serial, text, json } from "drizzle-orm/pg-core";
+import {
+  boolean,
+  integer,
+  pgTable,
+  serial,
+  text,
+  timestamp,
+} from "drizzle-orm/pg-core";
 
-export const demoTable = pgTable("demo_table", {
-  id: serial("id").primaryKey().notNull(),
-  demoName: text("demo_name").notNull(),
+export const countries = pgTable("countries", {
+  id: serial("id").notNull().primaryKey(),
+  iso: text("iso").notNull(),
+  locale: text("locale"),
+  name: text("name").notNull(),
+  nick_name: text("nick_name"),
+  phone_code: text("phone_code").notNull(),
+  flag: text("flag"),
 });
 
-export const sohamTable = pgTable("soham_table", {
-  id: serial("id").primaryKey().notNull(),
-  sohamName: text("soham_name", { enum: ["soham", "ganmote"] })
-    .notNull()
-    .unique(),
-  jsonSoham: json("json_soham").default({ soham: "name" }),
+export const billing = pgTable("billing", {
+  id: serial("id").notNull().primaryKey(),
+});
+
+export const users = pgTable("users", {
+  id: serial("id").notNull().primaryKey(),
+  username: text("username").notNull(),
+  first_name: text("first_name").notNull(),
+  last_name: text("last_name"),
+  email_id: text("email_id").notNull().unique(),
+  country_id: serial("country_id")
+    .references(() => countries.id)
+    .notNull(),
+  password: text("password").notNull(),
+  profile_image: text("profile_image"),
+  mode_of_signup: text("mode_of_signup").default(""),
+  role: text("role").default("user"),
+  two_factor_recovery_code: text("two_factor_recovery_code").notNull(),
+  dark_theme: boolean("dark_theme").default(false).notNull(),
+  admin_approval: boolean("admin_approval").default(true).notNull(),
+  is_active: boolean("is_active").default(true).notNull(),
+  remember_token: text("remember_token").default("").notNull(),
+  trial_end_time: timestamp("trial_end_time"),
+  billing_id: integer("billing_id").references(() => billing.id),
+  email_notification: boolean("email_notification").notNull().default(true),
+  gender: text("gender").default("male"),
+  tow_factor_auth: boolean("tow_factor_auth").default(false),
+  otp_id: text("otp_id").unique(),
 });
