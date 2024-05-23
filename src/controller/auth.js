1223d0ee1cb3459f2268ef1ecb0d9f31718f7e94
Yasin Bhojani <yasinbhojani333@gmail.com>
2024-05-23 17:32:44 +0530

    duplicate email check, endpoints and router changed, removed secret attributes

--- src/controller/auth.js
+++ src/controller/auth.js
@@ -2,8 +2,7 @@ import bcrypt from "bcrypt";
 import Users from "../models/users.model.js";
 import generateAccessToken from "../utils/token.util.js";
 
-//* Normal Auth
-export const normalLoginHandler = async (req, res) => {
+export const loginHandler = async (req, res) => {
   try {
     const { email, username, password } = req.body;
 
@@ -14,6 +13,9 @@ export const normalLoginHandler = async (req, res) => {
         where: {
           email: email.toLowerCase(),
         },
+        attributes: {
+          exclude: ["createdAt", "updatedAt"],
+        },
       });
     }
 
@@ -22,15 +24,20 @@ export const normalLoginHandler = async (req, res) => {
         where: {
           user_name: username.toLowerCase(),
         },
+        attributes: {
+          exclude: ["createdAt", "updatedAt"],
+        },
       });
     }
 
-    if (findCredentials.length <= 0) return res.status(200).json({ message: "wrong credentials" });
+    if (findCredentials.length <= 0) return res.status(400).json({ message: "wrong credentials" });
 
     bcrypt.compare(password, findCredentials[0].password, (err, result) => {
-      if (err) return res.json({ message: "An error occured" });
-      if (!result) return res.json({ message: "Incorrect Password" });
+      if (err) return res.status(500).json({ message: "An error occured" });
+      if (!result) return res.status(400).json({ message: "Incorrect Password" });
       else {
+        // delete encrypted pass from object
+        findCredentials[0].password = undefined;
         const accessToken = generateAccessToken(findCredentials[0]);
 
         return res.status(200).json({
@@ -46,74 +53,44 @@ export const normalLoginHandler = async (req, res) => {
     return res.status(500).json({ message: "Internal server error", error });
   }
 };
-export const normalRegistrationHandler = async (req, res) => {
+
+export const registrationHandler = async (req, res) => {
   try {
     const { first_name, last_name, email, password } = req.body;
+    // return if any of the variables are missing
+    if (!first_name || !last_name || !email || !password) {
+      return res.status(400).json({ message: "one or more fields missing for registration" });
+    }
 
     const user_name = `${first_name}_${last_name}`;
+
+    // check is email already exists
+    const accounts = await Users.findAll({
+      where: {
+        email: email.trim(),
+      },
+    });
+    if (accounts?.length !== 0) {
+      return res.status(400).json({ message: "email already in use" });
+    }
+
     const hashedPassword = await bcrypt.hash(password, 10);
 
     const user = { user_name, first_name, last_name, email };
     const token = generateAccessToken(user);
 
     const resp = await Users.create({
-      user_name: user_name.toLowerCase(),
-      first_name: first_name.toLowerCase(),
-      last_name: last_name.toLowerCase(),
-      email: email.toLowerCase(),
+      user_name: user_name.trim().toLowerCase(),
+      first_name: first_name.trim(),
+      last_name: last_name.trim(),
+      email: email.trim().toLowerCase(),
       password: hashedPassword,
     });
 
+    // removing password hash from response body
+    resp.password = undefined;
+
     res.status(200).json({ message: "success", token, result: resp });
-  } catch (error) {
-    return res.status(500).json({ message: "Internal server error", error });
-  }
-};
-
-//* Github Auth
-export const githubLoginHandler = (req, res) => {
-  try {
-    return res.status(200).json({ message: "success" });
-  } catch (error) {
-    return res.status(500).json({ message: "Internal server error", error });
-  }
-};
-export const githubRegistrationHandler = (req, res) => {
-  try {
-    return res.status(200).json({ message: "success" });
-  } catch (error) {
-    return res.status(500).json({ message: "Internal server error", error });
-  }
-};
-
-//* Microsoft Auth
-export const microsoftLoginHandler = (req, res) => {
-  try {
-    return res.status(200).json({ message: "success" });
-  } catch (error) {
-    return res.status(500).json({ message: "Internal server error", error });
-  }
-};
-export const microsoftRegistrationHandler = (req, res) => {
-  try {
-    return res.status(200).json({ message: "success" });
-  } catch (error) {
-    return res.status(500).json({ message: "Internal server error", error });
-  }
-};
-
-//* Google Auth
-export const googleLoginHandler = (req, res) => {
-  try {
-    return res.status(200).json({ message: "success" });
-  } catch (error) {
-    return res.status(500).json({ message: "Internal server error", error });
-  }
-};
-
-export const googleRegistrationHandler = (req, res) => {
-  try {
-    return res.status(200).json({ message: "success" });
   } catch (error) {
     return res.status(500).json({ message: "Internal server error", error });
   }
